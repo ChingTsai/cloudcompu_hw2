26a6c010006f4c87417bdf5b3dafa3d29ef157d6
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-05-16 04:46:25 +0800

    Fix compu error

--- PageRankMr/PageRankMr/src/main/java/cloudCompu/PageRankMr/CompuErrMapper.java
+++ PageRankMr/PageRankMr/src/main/java/cloudCompu/PageRankMr/CompuErrMapper.java
@@ -13,13 +13,19 @@ public class CompuErrMapper extends Mapper<Text, Text, Text, DoubleWritable> {
 	public void map(Text key, Text value, Context context) throws IOException,
 			InterruptedException {
 		String[] detial = value.toString().split("&gt;");
-		String[] par = detial[0].split(" ");
+		if (detial.length == 1) {
+			System.out.println(key);
+			title.set(key);
+			dis.set(9.9);
+			context.write(title, dis);
+		} else {
+			String[] par = detial[0].split(" ");
 
-		title.set("Sum");
-		dis.set(Math.abs(Double.parseDouble(par[0])
-				- Double.parseDouble(par[1])));
-		context.write(title, dis);
-
+			title.set("Sum");
+			dis.set(Math.abs(Double.parseDouble(par[0])
+					- Double.parseDouble(par[1])));
+			context.write(title, dis);
+		}
 	}
 
 }
