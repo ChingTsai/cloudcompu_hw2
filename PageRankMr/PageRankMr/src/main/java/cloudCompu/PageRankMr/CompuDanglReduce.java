f100d63adbd27f58fa8b5d20b14279e56155a280
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-05-16 06:21:34 +0800

    Fix comput error

--- PageRankMr/PageRankMr/src/main/java/cloudCompu/PageRankMr/CompuDanglReduce.java
+++ PageRankMr/PageRankMr/src/main/java/cloudCompu/PageRankMr/CompuDanglReduce.java
@@ -13,14 +13,13 @@ public class CompuDanglReduce extends Reducer<Text, DoubleWritable, Text, Text> 
 	public void reduce(Text key, Iterable<DoubleWritable> values,
 			Context context) throws IOException, InterruptedException {
 		double tmppr = 0.0d;
-		double alpha = context.getConfiguration().getDouble("alpha", 0.85);
 
 		for (DoubleWritable val : values) {
 			tmppr += val.get();
 		}
 
 		title.set("Sum");
-		pr.set(String.valueOf(tmppr * alpha));
+		pr.set(String.valueOf(tmppr ));
 		context.write(title, pr);
 	}
 }
