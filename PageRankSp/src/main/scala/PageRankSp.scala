69b155f41d16b9656043a39355a1b132d58f3f8f
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-05-15 12:44:56 +0800

    Fix filter bug

--- PageRankSp/src/main/scala/PageRankSp.scala
+++ PageRankSp/src/main/scala/PageRankSp.scala
@@ -39,7 +39,7 @@ object PageRankSp {
         //
         .map(row => {
           row._2.toArray.filter(_ != "&gt").map(tp => (tp, row._1)).+:(row._1, "&gt");
-        }).flatMap(y => y).groupByKey(sc.defaultParallelism * 10).map(x => (x._1, x._2.toArray.filter { _ == "&gt" }));
+        }).flatMap(y => y).groupByKey(sc.defaultParallelism * 10).map(x => (x._1, x._2.toArray.filter { _ != "&gt" }));
 
     link.cache();
 
