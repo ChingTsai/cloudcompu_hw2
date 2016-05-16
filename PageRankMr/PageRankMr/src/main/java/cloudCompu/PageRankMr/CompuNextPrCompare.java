107c6cdfb685a632dfaa87fc0d57463627f3386e
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-05-16 15:10:54 +0800

    Gora

--- PageRankMr/PageRankMr/src/main/java/cloudCompu/PageRankMr/CompuNextPrCompare.java
+++ PageRankMr/PageRankMr/src/main/java/cloudCompu/PageRankMr/CompuNextPrCompare.java
@@ -18,7 +18,7 @@ public class CompuNextPrCompare extends WritableComparator {
 		Double err1 = Double.parseDouble(s1[1]);
 		Double err2 = Double.parseDouble(s2[1]);
 		if (Double.compare(err1, err2) == 0) {
-			return s1[0].compareTo(s2[0]);
+			return -1*s1[0].compareTo(s2[0]);
 		} else {
 			return err1.compareTo(err2);
 		}
