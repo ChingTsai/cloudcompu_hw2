bd6973463b8f209bbc54af816c2bb8167ed7646d
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-05-18 01:28:12 +0800

    revert Scala v2

--- PageRankSp/src/main/scala/PageRankSp.scala
+++ PageRankSp/src/main/scala/PageRankSp.scala
@@ -48,8 +48,7 @@ object PageRankSp {
     var micros = (System.nanoTime - st) / 1000000000.0
     System.out.println("Parse :  %1.5f seconds".format(micros));
 
-    //var rddPR = link.map(x => (x._1, (x._2, 1.0 / n)));
-    var rddPR = link.map( x => (x._1,1.0));
+    var rddPR = link.map(x => (x._1, (x._2, 1.0 / n)));
 
     var Err = 1.0;
     var iter = 0;
@@ -59,16 +58,6 @@ object PageRankSp {
     while (Err > 0.001) {
       st = System.nanoTime
 
-      var con = link.join(rddPR);
-      val dangpr = con.filter(_._2._1.length == 0).map(_._2._2).reduce(_ + _) / n * alpha;
-      val tmpPR = con.map(row => {
-
-        row._2._1.map { tp => (tp, row._2._2 / row._2._1.length * alpha) }.+:(row._1, 1.0 / n * (1 - alpha) + dangpr);
-
-      }).flatMap(y => y).reduceByKey(_ + _);
-      //var Er = tmpPR.subtractByKey(rddPR.map(x => (x._1, x._2._2))).partitionBy(partitioner)
-      Err = tmpPR.subtractByKey(rddPR).map(_._2.abs).reduce(_ + _);
-      /*
       val dangpr = rddPR.filter(_._2._1.length == 0).map(_._2._2).reduce(_ + _) / n * alpha;
       //System.out.println("Dangl :  %1.10f ".format(dangpr));
 
@@ -80,23 +69,14 @@ object PageRankSp {
       //var Er = tmpPR.subtractByKey(rddPR.map(x => (x._1, x._2._2))).partitionBy(partitioner)
       Err = (tmpPR.join(rddPR.map(x => (x._1, x._2._2)), sc.defaultParallelism * 8)).map(x => (x._2._1 - x._2._2).abs).reduce(_ + _);
       rddPR = rddPR.map(x => (x._1, x._2._1)).join(tmpPR, sc.defaultParallelism * 8);
-			*/
-      
-      
-      
+
       micros = (System.nanoTime - st) / 1000000000.0
       System.out.println("Iteration : " + iter + " err: " + Err);
       System.out.println("Compute :  %1.5f seconds".format(micros))
       iter = iter + 1;
-      
-      
-      
-      
-      
-      
     }
 
-    val res = rddPR;
+    val res = rddPR.map(x => (x._1, x._2._2));
     res.sortBy({ case (page, pr) => (-pr, page) }, true, sc.defaultParallelism * 10).map(x => x._1 + "\t" + x._2).saveAsTextFile(outputPath);
 
     sc.stop
